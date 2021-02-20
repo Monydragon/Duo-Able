d19e406d6d1331466e0af8992c0e081c82bd7972
Jeffery Miller <Mony@dragonlensstudios.com>
2021-02-20 00:25:56 -0600

    Added new Tilemap/Playeranim
    Added player animation and sprite, added new tiles and several other assets

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,13 @@ public class Player : MonoBehaviour
     public InputDevice Device { get; set; }
     public float speed = 2f;
     public Vector3 movement;
+    private Animator anim;
+    public Vector3 lastMove;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +27,20 @@ public class Player : MonoBehaviour
     void Update()
     {
         movement = new Vector3(Device.LeftStick.X, Device.LeftStick.Y);
+        anim.SetFloat("MoveX", Device.LeftStick.X);
+        anim.SetFloat("MoveY", Device.LeftStick.Y);
+        if(movement != Vector3.zero)
+        {
+            anim.SetBool("isMoving", true);
+            lastMove = new Vector3(Device.LeftStick.X, Device.LeftStick.Y);
+            anim.SetFloat("LastMoveX", lastMove.x);
+            anim.SetFloat("LastMoveY", lastMove.y);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+
         transform.position += movement * Time.deltaTime * speed;
         
         if (Device.Action1.WasPressed)
